bb0ee386013eeefab300e9356c4dff03ab6e88db
Pourya <pourya.nejadensan@gmail.com>
2024-08-22 11:57:20 +0200

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,44 @@
+import {Todo} from "./Todo.ts";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import TodoColumn from "./TodoColumn.tsx";
+import {allPossibleTodos} from "./TodoStatus.ts";
+
+function App() {
+
+    const [todos, setTodos] = useState<Todo[]>()
+
+    function fetchTodos() {
+        axios.get("/api/todo")
+            .then(response => {
+                setTodos(response.data)
+            })
+    }
+
+    useEffect(fetchTodos, [])
+
+    if (!todos) {
+        return "Lade..."
+    }
+
+    return (
+        <>
+            <div className="page">
+                <h1>TODOs</h1>
+                {
+                    allPossibleTodos.map(status => {
+                        const filteredTodos = todos.filter(todo => todo.status === status)
+                        return <TodoColumn
+                            status={status}
+                            todos={filteredTodos}
+                            onTodoItemChange={fetchTodos}
+                            key={status}
+                        />
+                    })
+                }
+            </div>
+        </>
+    )
+}
+
+export default App
